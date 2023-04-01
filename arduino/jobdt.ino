1aa0ae528b849ced8d49aa83908ad95dc096f874
AripJam18 <51983099+AripJam18@users.noreply.github.com>
2023-04-01 16:02:27 +0700

    Update jobdt.ino

--- arduino/jobdt.ino
+++ arduino/jobdt.ino
@@ -1,9 +1,12 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <RGBmatrixPanel.h>
+#include "RTClib.h"
+RTC_DS3231 rtc;
 //kondisi Button saat diTekan:
 //1. Button Start = ketika Standby dia HIGH(1)  dan  ketika  ditekan dia LOW(0)
 //2. Button StOP = ketika Standby dia LOW(0)  dan  ketika  ditekan dia HIGH(1)
+//setCursor(Baris,Kolom);
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //MAC Address
 String DataDuration;
@@ -16,7 +19,9 @@ EthernetClient client;
 
 const int Start =6;
 const int Stop =7;
-const int lampu =4;
+const int hijau =3;
+const int kuning =4;
+const int merah =5;
 #define CLK 11 // USE THIS ON ARDUINO MEGA
 #define OE   9
 #define LAT 10
@@ -25,28 +30,29 @@ const int lampu =4;
 #define C   A2
 #define D   A3
 
+int Ms = 00; // count hours
 int S = 00; // count seconds 
 int M = 00; // count minutes
-int H = 00; // count hours
 String getS;
 String getM;
-String getH; //ini untuk kirim jam, menit , detik dari stopwatch
-
+String getMs;
+String jam,menit,detik;
 int mulai=0;
 int berhenti=0;
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false,64); 
 
 
-
 void setup() {
   Serial.begin(9600);
-  Ethernet.begin(mac, ip);
-  Serial.print("Local IP: ");
-  Serial.println(Ethernet.localIP());
+//  Ethernet.begin(mac, ip);
+//  Serial.print("Local IP: ");
+//  Serial.println(Ethernet.localIP());
   pinMode(Start, INPUT_PULLUP);
   pinMode(Stop, INPUT_PULLUP);
-  pinMode(lampu, OUTPUT); 
+  pinMode(merah, OUTPUT); 
+  pinMode(kuning, OUTPUT); 
+  pinMode(hijau, OUTPUT); 
   matrix.begin(); 
   matrix.fillScreen(0); //to refresh
   matrix.setTextColor(matrix.Color333(7,0,0));
@@ -56,6 +62,15 @@ void setup() {
   matrix.print("00");
   matrix.print(":");
   matrix.print("00");
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    }
   delay(2000);
 }
 
@@ -63,7 +78,7 @@ void setup() {
 
 
 void loop() {
-
+Jam();
 if (digitalRead(Start) == LOW) {
     mulai=1;
     berhenti=0;
@@ -82,13 +97,16 @@ else if ((mulai == 0) && (berhenti == 1)) {
   matrix.setCursor(2,0);
   matrix.setTextColor(matrix.Color333(7,0,0));
   //matrix.print("Kirim Data");
-  SendtoDB();
+  //SendtoDB();
   delay(1000);
-  H=00; 
+  Ms=00; 
   M=00; 
   S=00;
   berhenti=0;
   mulai=0;
+  digitalWrite(hijau,LOW);
+  digitalWrite(kuning,LOW);
+  digitalWrite(merah,LOW);
  }
 
 }
@@ -96,10 +114,17 @@ else if ((mulai == 0) && (berhenti == 1)) {
 
 void Stopwatch(){
   //------------------------------CHANGE TIME---------------------------------
-    S++;
-    delay(1000);
+    digitalWrite(hijau,HIGH);
+    Ms++;
+    delay(10);
     matrix.fillScreen(0); //to refresh
     matrix.setTextColor(matrix.Color333(7,0,0)); 
+    if (Ms>25) //if miliseconds are up,
+    {
+      S++; // to addup minute by one
+      Ms=0; // reset second section to 0
+    }
+
     if (S>59) //if seconds are up,
     {
       M++; // to addup minute by one
@@ -108,33 +133,45 @@ void Stopwatch(){
 
     if (M>59) //if minutes are up,
     {
-      H++; //reduce hour by one
       M=0; //reset minute to 59
     }
     //----------------------------DISPLAY TIME----------------------------- 
     matrix.setCursor(8,12);
     matrix.setTextColor(matrix.Color333(7,0,0)); 
-    if (S <10 && S>=1){ 
-      digitalWrite(lampu,HIGH);
+       if (M <10 && M>=0){ 
+      digitalWrite(hijau,HIGH);
+      digitalWrite(kuning,LOW);
+      digitalWrite(merah,LOW); 
+      //LAMPU HIJAU      
+    } else if (M <15 && M>=10){ 
+      digitalWrite(hijau,LOW);
+      digitalWrite(kuning,HIGH);
+      digitalWrite(merah,LOW); 
       //LAMPU HIJAU      
     } 
-     else if (S <15 && S>=10){
-      digitalWrite(lampu,HIGH);
+     else if (M <59 && M>=16){
+      digitalWrite(hijau,LOW);
+      digitalWrite(kuning,LOW);
+      digitalWrite(merah,HIGH);
       //LAMPU ORANGE
-    } else{
-      //LAMPU MERAH      
-    }
+    } 
+//else if ( S <59 && S>=16){
+//      //LAMPU MERAH  
+//      digitalWrite(hijau,HIGH);
+//      digitalWrite(kuning,LOW);
+//      digitalWrite(merah,HIGH);    
+//    }
    
-    if (H < 10){
-      matrix.print("0"); //Extra space is to ensure colons are in the same spot when theres only 1 digit
-      matrix.print(H);
-      getH ="0" + String(H);
-    } else {
-      matrix.print(H);
-      getH =H;
-    }
-    
-    matrix.print(":");
+//    if (H < 10){
+//      matrix.print("0"); //Extra space is to ensure colons are in the same spot when theres only 1 digit
+//      matrix.print(H);
+//      getH ="0" + String(H);
+//    } else {
+//      matrix.print(H);
+//      getH =H;
+//    }
+//    
+//    matrix.print(":");
 
     if (M < 10){
       matrix.print("0"); //Extra space is to ensure colons are in the same spot when theres only 1 digit
@@ -158,7 +195,13 @@ void Stopwatch(){
       Serial.println(S);
       getS =S; 
     }
-    DataDuration =getH+":"+getM+":"+getS;
+
+    matrix.print(":");
+    matrix.print(Ms);
+    Serial.println(Ms);
+    getMs =Ms; 
+    
+    DataDuration =getM+":"+getS+":"+getMs;
 
     if (M > 15){
       DataRemarks = "Late";
@@ -167,8 +210,19 @@ void Stopwatch(){
     }
     
 }
-
-
+void Jam(){
+  DateTime now = rtc.now();
+  jam = now.hour();
+  menit =now.minute();
+  detik = now.second();
+  matrix.setCursor(8,1);
+  matrix.setTextColor(matrix.Color333(7,0,0)); 
+  matrix.print(jam);
+  matrix.print(":"); 
+  matrix.print(menit);
+  matrix.print(":");
+  matrix.print(detik);
+}
 void SendtoDB(){
   if (client.connect(server, 80)) {
     Serial.println("");
